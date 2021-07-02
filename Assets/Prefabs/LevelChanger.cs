5bc7958dd7305d7a68ddd4a4b02b8008ab2285c3
Astalios <37106010+Astalios@users.noreply.github.com>
2021-07-02 07:37:10 +0200

    ajout game_over screen canvas + script level changer

---
+++ Assets/Prefabs/LevelChanger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Button monBtn;
+
+    void Start()
+    {
+        Button btn = monBtn.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(EventSystem.current.currentSelectedGameObject.name);
+    }
+}
